ca019dd96925dd2225a95e5952fcd628d91f3779
amirGH987 <ghasemianamir0@gmail.com>
2025-12-22 20:48:30 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
 document.getElementById("reserveForm").addEventListener("submit", function(e) {
   e.preventDefault();
-  window.open("https://wa.me/989102624560", "_blank");
-});
+
+  const name = document.getElementById("name").value;
+  const phone = document.getElementById("phone").value;
+  const massage = document.getElementById("massage").value;
+  const date = document.getElementById("date").value;
+  const time = document.getElementById("time").value;
+
+  if (!name  !phone  !massage  !date  !time) {
+    alert("لطفاً همه فیلدها را کامل پر کنید");
+    return;
+  }
+
+  const text =
+    "رزرو نوبت ماساژ\n\n" +
+    "نام: " + name + "\n" +
+    "شماره تماس: " + phone + "\n" +
+    "خدمات: " + massage + "\n" +
+    "تاریخ: " + date + "\n" +
+    "ساعت: " + time;
+
+  const whatsappNumber = "989102624560"; // 👈 شماره واقعی خودت
+  const link =
+    "https://wa.me/" +
+    whatsappNumber +
+    "?text=" +
+    encodeURIComponent(text);
+
+  location.href = link; // ✅ راه درست برای موبایل
+});;
