ce94caa09c734436b6e98f03af6f7fa9dcb6bb24
amirGH987 <ghasemianamir0@gmail.com>
2025-12-22 20:37:30 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
 document.getElementById("reserveForm").addEventListener("submit", function(e) {
   e.preventDefault();
-
-  let name = document.getElementById("name").value;
-  let phone = document.getElementById("phone").value;
-  let massage = document.getElementById("massage").value;
-  let date = document.getElementById("date").value;
-  let time = document.getElementById("time").value;
-
-  let text =
-    رزرو نوبت ماساژ\n\n +
-    نام: ${name}\n +
-    شماره: ${phone}\n +
-    نوع ماساژ: ${massage}\n +
-    تاریخ: ${date}\n +
-    ساعت: ${time};
-
-  let whatsappNumber = "989102624560"; // ← شماره خودت
-  let url = https://wa.me/${whatsappNumber}?text=${encodeURIComponent(text)};
-
-  window.open(url, "_blank");
+  alert("JS وصل شد");
 });
